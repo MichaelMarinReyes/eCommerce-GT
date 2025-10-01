213df2f7eac87023413ebb5030f8734444ebd1a4
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-01 11:51:55 -0600

    Creación del proyecto y repositorio

---
+++ eCommerce GT/Backend/eCommerceGt/src/main/java/principal/ECommerceGtApplication.java
@@ -0,0 +1,13 @@
+package principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceGtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceGtApplication.class, args);
+    }
+
+}
