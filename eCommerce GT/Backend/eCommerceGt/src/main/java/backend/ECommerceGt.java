8223edcb52b1fd6ef5ac1b4fdeda6f6cdb321674
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-05 14:11:10 -0600

    Diagrama de clases, no terminado

--- eCommerce GT/Backend/eCommerceGt/src/main/java/backend/ECommerceGt.java
+++ eCommerce GT/Backend/eCommerceGt/src/main/java/backend/ECommerceGt.java
@@ -1,13 +1,12 @@
-package principal;
+package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceGtApplication {
+public class ECommerceGt {
 
     public static void main(String[] args) {
-        SpringApplication.run(ECommerceGtApplication.class, args);
+        SpringApplication.run(ECommerceGt.class, args);
     }
-
 }
