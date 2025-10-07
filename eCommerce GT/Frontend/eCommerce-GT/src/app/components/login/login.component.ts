059f478c4910325709bcdf9fb177557cf12754f0
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-07 12:19:45 -0600

    Vistas de página principal, login y registro

---
+++ eCommerce GT/Frontend/eCommerce-GT/src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'login',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  userName: string = '';
+  email: string = '';
+  password: string = '';
+  passwordVisible = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    if (!this.email?.trim() || !this.password?.trim()) {
+      alert("Debe llenar ambos campos para iniciar sesión");
+      return;
+    }
+
+    alert(`Bienvenido, ${this.email}`);
+    this.router.navigate(['/start-page']);
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+}
