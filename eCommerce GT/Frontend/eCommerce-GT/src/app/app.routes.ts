897cba5287de9d45290ae9d8bc7f501bfa875b0d
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-18 15:16:46 -0600

    Correcciones en frontend y vista principal de usuario común

--- eCommerce GT/Frontend/eCommerce-GT/src/app/app.routes.ts
+++ eCommerce GT/Frontend/eCommerce-GT/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { CommonUserComponent } from './components/common-user/common-user.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: '', title: 'eCommerce-GT', component: MainPageComponent },
+    { path: 'home', title: 'eCommerce-GT', component: MainPageComponent },
+    { path: 'login', title: 'Iniciar sesión', component: LoginComponent },
+    { path: 'register', title: 'Registrarase', component: RegisterComponent },
+    { path: 'common-user', title: 'eCommerce-GT', component: CommonUserComponent }
 ];
