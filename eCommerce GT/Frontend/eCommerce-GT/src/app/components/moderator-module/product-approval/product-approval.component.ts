dcd45c9e8421df7777a757469a03aa9b8a5eb6e7
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-19 21:09:33 -0600

    Creación de componentes en frontend para las vistas, no están todas

---
+++ eCommerce GT/Frontend/eCommerce-GT/src/app/components/moderator-module/product-approval/product-approval.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-approval',
+  imports: [],
+  templateUrl: './product-approval.component.html',
+  styleUrl: './product-approval.component.css'
+})
+export class ProductApprovalComponent {
+
+}
---
+++ eCommerce GT/Frontend/eCommerce-GT/src/app/components/user-module/rating-comment/rating-comment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-comment',
+  imports: [],
+  templateUrl: './rating-comment.component.html',
+  styleUrl: './rating-comment.component.css'
+})
+export class RatingCommentComponent {
+
+}
