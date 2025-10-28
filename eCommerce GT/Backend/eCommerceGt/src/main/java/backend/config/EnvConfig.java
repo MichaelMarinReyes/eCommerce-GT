3802226478e9b765e1c156c9c5424396070376d4
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2025-10-28 02:16:28 -0600

    Corrección al fallo de seguridad de la llave para enviar correos

---
+++ eCommerce GT/Backend/eCommerceGt/src/main/java/backend/config/EnvConfig.java
@@ -0,0 +1,33 @@
+package backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+    static {
+        // IMPRIME RUTA ACTUAL
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("Directorio actual: " + currentDir);
+
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("./")   // ← BUSCA EN LA RAÍZ DEL PROYECTO
+                    .filename(".env")
+                    .ignoreIfMissing()
+                    .load();
+
+            String key = dotenv.get("BREVO_SMTP_KEY");
+            if (key != null && !key.isEmpty()) {
+                System.setProperty("BREVO_SMTP_KEY", key);
+                System.out.println("CLAVE SMTP CARGADA: " + key.substring(0, 20) + "...");
+            } else {
+                System.out.println("ERROR: BREVO_SMTP_KEY no encontrada en .env");
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERROR AL CARGAR .env: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
